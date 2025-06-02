f6725bcccb61014eb1ae710cec579727bfc08f10
Keshav <143893323+keshav23456@users.noreply.github.com>
2025-06-02 22:50:04 +0530

    Add perspective utility to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -69,6 +69,9 @@ export default {
         'inner-lg': 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
         'soft-xl': '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
       },
+      perspective: {
+        '1000': '1000px',
+      },
     },
   },
   plugins: [],
